f44bc3fa0d6bf4226697f39d053bed75a2ec8c5e
Med <mehdi.gacem@versusmind.eu>
2019-09-21 16:12:25 +0200

    initial commit

---
+++ src/main/java/com/jpa/shoolmarks/ShoolmarksApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.shoolmarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoolmarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoolmarksApplication.class, args);
+	}
+
+}
