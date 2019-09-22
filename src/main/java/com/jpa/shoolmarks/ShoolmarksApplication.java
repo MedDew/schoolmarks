0ec779459327addb74eb1ca54b7c56f6ee70531e
Med <mehdi.gacem@versusmind.eu>
2019-09-22 20:17:43 +0200

    add the spring version

--- src/main/java/com/jpa/shoolmarks/ShoolmarksApplication.java
+++ src/main/java/com/jpa/shoolmarks/ShoolmarksApplication.java
@@ -2,11 +2,13 @@ package com.jpa.shoolmarks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class ShoolmarksApplication {
 
 	public static void main(String[] args) {
+		System.out.println("SPRING VERSION: " + SpringVersion.getVersion());
 		SpringApplication.run(ShoolmarksApplication.class, args);
 	}
 
