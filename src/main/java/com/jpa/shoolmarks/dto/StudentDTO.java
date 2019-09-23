dd1138f9f0bc4dc8df82c371fa8a0b5f897a317a
Med <mehdi.gacem@versusmind.eu>
2019-09-23 17:16:00 +0200

    add a no argumet constructor annotation

--- src/main/java/com/jpa/shoolmarks/dto/StudentDTO.java
+++ src/main/java/com/jpa/shoolmarks/dto/StudentDTO.java
@@ -2,13 +2,18 @@ package com.jpa.shoolmarks.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class StudentDTO {
     private long id;
     private String name;
     private int age;
+    //private List<MarksDTO> marks;
 }
