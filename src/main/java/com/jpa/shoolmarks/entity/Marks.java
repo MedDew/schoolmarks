c85475c9c23f1dc0e5cf30fbb87db623ed52120c
Med <mehdi.gacem@versusmind.eu>
2019-09-23 10:03:49 +0200

    change the generation type value

--- src/main/java/com/jpa/shoolmarks/entity/Marks.java
+++ src/main/java/com/jpa/shoolmarks/entity/Marks.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 public class Marks {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
